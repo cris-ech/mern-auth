ec16841af5442f6c708234953931d6b94f4eebb7
cris-ech <i42runac@uco.es>
2019-03-20 18:52:58 +0100

    Basic backend

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 //initialize the app using express
 
@@ -23,6 +25,13 @@ mongoose.connect(
 )
 .then(() => console.log("MongoDB successfully connected"))
 .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
